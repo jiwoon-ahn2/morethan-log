2dbfdb072da3777b07b2524c3c414a98d6a59b93
jiwoon-ahn2 <jyun3131@gmail.com>
2026-02-05 11:39:11 +0900

    Update index.tsx

--- src/routes/Feed/index.tsx
+++ src/routes/Feed/index.tsx
@@ -95,7 +95,7 @@ const StyledWrapper = styled.div`
     grid-column: span 12 / span 12;
 
     @media (min-width: 1024px) {
-      grid-column: span 7 / span 7;
+      grid-column: span 9 / span 7;
     }
 
     > .tags {
@@ -136,6 +136,7 @@ const StyledWrapper = styled.div`
     }
   }
 `
+
 
 
 
